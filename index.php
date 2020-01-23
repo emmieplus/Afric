bc9b1cbac5b08defd8797fb272f4c593c3533d62
emmieplus <emmieplus@gmail.com>
2020-01-23 14:07:37 +0100

    Inex title modified
    Signed-off-by: emmieplus <emmieplus@gmail.com>

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 		<!-- Other Header -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Health Afric</title>
+		<title>Health Afric | NIgeria</title>
 	</head>
 	<body>
 		<div class="container">
